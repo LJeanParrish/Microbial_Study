5405b972ca09c6ea4956a165d7b2015014ad697c
Lauren Parrish <ljeanparrish@gmail.com>
2021-04-06 11:16:19 -0400

    updates to code

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+////////HORIZONTAL BAR CHART//////
+//SAMPLES object contains otu_id, sample_values, and otu_labels
+//samples.id index [0]
+//samples.otu_ids [1]
+//samples.sample_values [2]
+//samples.otu_labels [3]
+
+
 function getPlots(id) {
   //connect app.js to the samples.json file
-  d3.json("data/samples.json").then((data) => {
+  d3.json("data/samples.json").then(data => {
   //confirm data reading in correctly
-  console.log(data);
+  console.log(data)
+
+  //create variable to bring in out_ids
+  var ids = data.samples[1].otu_ids;
+  console.log(ids)
+
+  //create variable for sample_values
+  var sample_val = data.samples[2].sample_val;
+  console.log(sample_val)
+
+  //create variable for otu_labels
+  var label = data.samples[3].otu_labels;
+  console.log(label)
+
+
 } 
 
   
