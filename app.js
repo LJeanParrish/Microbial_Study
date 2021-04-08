c04cc1d549ebe04eaf18ea4b9fb20d7334a94cac
Lauren Parrish <ljeanparrish@gmail.com>
2021-04-08 16:06:29 -0400

    updated code

--- app.js
+++ app.js
@@ -5,36 +5,60 @@
 //samples.sample_values [2]
 //samples.otu_labels [3]
 
+function dropdownmenu() {
+  d3.json("data/samples.json").then(data => {
+    var samplenames = data.names
+    console.log(samplenames)
+    var menu = d3.select("#selDataset")
+    samplenames.forEach((x) => {
+      menu.append("option").text(x).property("value")
 
-function init() {
+
+    })
+    buildcharts(samplenames[0])
+  })
+}
+
+function buildtable(newid) {}
+
+function optionChanged(newid) {
+buildtable(newid)
+buildcharts(newid)
+}
+
+function buildcharts(sampleid) {
   //connect app.js to the samples.json file
   d3.json("data/samples.json").then(data => {
     console.log(data)
 
+    var filterdata = data.samples.filter(x => x.id == sampleid)[0]
+    console.log(filterdata)
+
     //create variable to bring in out_ids
-    var ids = data.samples[1].otu_ids;
+    var ids = filterdata.otu_ids;
     console.log(ids)
 
     //create variable for sample_values
-    var sample_val = data.samples[2].sample_val;
-    console.log(sample_val)
+    var sample_val = filterdata.sample_values;
+    console.log(sample_val.slice(0,10))
 
     //create variable for otu_labels
-    var label = data.samples[3].otu_labels;
-    console.log(label)
+    var label = filterdata.otu_labels;
+    // console.log(label)
 
     //isolate the top 10 sample values
-    var top_10 = sample_val.slice(0, 10).reverse()
+    // var top_10 = sample_val.slice(0, 10).reverse()
 
     //create trace for horizontal bar chart
     var trace1 = {
-      x: sample_val,
-      y: top_10,
-      text: labels,
+      x: sample_val.slice(0,10).reverse(),
+      y: ids.slice(0,10).reverse(),
+      text: label.slice(0,10).reverse(),
       marker: {
         color: 'blue'
       },
-      type: "bar"
+      type: "bar",
+      orientation :"h"
     };
 
     //create the data array for the plot
@@ -46,61 +70,68 @@ function init() {
     };
 
     //use plotly to create new par chart
-    Plotly.newPlot('plot', data, layout);
+    Plotly.newPlot('bar', data, layout);
 
-///////////Create code for dropdown menus///////////////////////////////
-//Note HTML has been amended to apply dropdowns. 
-// Call updatePlotly() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", updatePlotly);
+    ///call new trace and new bubble layout can use same x and y
 
-// This function is called when a dropdown menu item is selected
-function updatePlotly() {
-  // Use D3 to select the dropdown menu
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var dataset = dropdownMenu.property("value");
+    
+  })
+}
 
-  /////////////////////////CONSTRUCT BUBLE CHART////////////////////////
-  var otus = data.samples[0].otu_ids;
-  var sample = data.samples[0].sample_values;
+    ///////////Create code for dropdown menus///////////////////////////////
+    //Note HTML has been amended to apply dropdowns. 
+    // Call updatePlotly() when a change takes place to the DOM
+    // d3.selectAll("#selDataset").on("change", updatePlotly);
 
-  var trace2 = {
-    x: otus,
-    y: sample,
-    mode: "markers",
-    marker: {
-      size: sample,
-      color: otus,
-      text: data.samples[0].otu_labels
-  },
+    // // This function is called when a dropdown menu item is selected
+    // function updatePlotly() {
+    //   // Use D3 to select the dropdown menu
+    //   var dropdownMenu = d3.select("#selDataset");
+    //   // Assign the value of the dropdown menu option to a variable
+    //   var dataset = dropdownMenu.property("value");
 
-    //create the data array for the plot
-    var data2 = [trace2];
+      /////////////////////////CONSTRUCT BUBLE CHART////////////////////////
+    //   var otus = data.samples[0].otu_ids;
+    //   var sample = data.samples[0].sample_values;
 
-    var layout2 = {
-      title: "Operational Taxonomic Units (OTUs)"
-    };
+    //   var trace2 = {
+    //     x: otus,
+    //     y: sample,
+    //     mode: "markers",
+    //     marker: {
+    //       size: sample,
+    //       color: otus,
+    //       text: data.samples[0].otu_labels
+    //     }
+    //   },
 
-    // create the bubble plot
-    Plotly.newPlot("bubble", data2, layout2);
-  };
+    //   //create the data array for the plot
+    //   var data2 = [trace2];
+
+    //   var layout2 = {
+    //     title: "Operational Taxonomic Units (OTUs)"
+    //   };
+
+    //   // create the bubble plot
+    //   Plotly.newPlot("bubble", data2, layout2);
+    // };
 
 
-  /////Create Metadata Table///////////////////////////////////
-  //Read metadata table pulling in demographic information
-  var metadata = data.metadata;
-  console.log(metadata)
+    /////Create Metadata Table///////////////////////////////////
+    //Read metadata table pulling in demographic information
+    // var metadata = data.metadata;
+    // console.log(metadata)
 
-  //create variables for other demographic info
-  var id = data.metadata.id;
-  var ethnicity = data.metadata.ethnicity;
-  var gender = data.metadata.gender;
-  var age = data.metadata.age;
-  var location = data.metadata.location;
-  var bbtype = data.metadata.bbtype;
-  var wfreq = data.metadata.wfreq;
+    // //create variables for other demographic info
+    // var id = data.metadata.id;
+    // var ethnicity = data.metadata.ethnicity;
+    // var gender = data.metadata.gender;
+    // var age = data.metadata.age;
+    // var location = data.metadata.location;
+    // var bbtype = data.metadata.bbtype;
+    // var wfreq = data.metadata.wfreq;
 
-init();
+      dropdownmenu();
 
 
 
