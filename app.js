9b116f9026d1b271ea5b6ec7f17fe7c0c87bf132
Lauren Parrish <ljeanparrish@gmail.com>
2021-04-08 18:00:31 -0400

    updated code for demographics table

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function buildcharts(sampleid) {
   d3.json("data/samples.json").then(data => {
     console.log(data)
 
-    var filterdata = data.samples.filter(x => x.id == sampleid)[0]
+    var filterdata = data.samples.filter(x => x.id == sampleid)[0];
     console.log(filterdata)
 
     //create variable to bring in out_ids
@@ -55,6 +55,7 @@ function buildcharts(sampleid) {
     //create the layout
     var layout = {
       title: "Top 10 Operational Taxonomic Units (OTUs)",
+
     };
 
     //use plotly to create new par chart
@@ -76,65 +77,35 @@ function buildcharts(sampleid) {
     var data2 = [trace2];
 
     var layout2 = {
-      title: "Operational Taxonomic Units (OTUs)"
+      title: "Operational Taxonomic Units (OTUs)",
+      height: 600,
+      width: 1000
     };
 
     // create the bubble plot
-    Plotly.newPlot("bubble", data2, layout2); 
-    
-    /////CREATE THE DEMOGRAPHICS TABLE//////////////////////////
+    Plotly.newPlot("bubble", data2, layout2);
+  })
+}
 
+/////CREATE THE DEMOGRAPHICS TABLE//////////////////////////
+function demoInfo(demoid) {
+  d3.json("data/samples.json").then(data => {
     var metadata = data.metadata;
     console.log(metadata);
     
-    // Get a reference to the html body
-    
-    var body = d3.select(".panel-body");
 
+    //filter demo information
+    var filteredDemo = metadata.filter(info => info.demoid.toString() === demoid)[0];
 
-})
-}
+    var panelBody = d3.select("#sample-metadata");
 
+    //empty the demo info panel each time before getting new data
+    panelBody.html("");
 
-/////Create Metadata Table///////////////////////////////////
-//Read metadata table pulling in demographic information
-// var metadata = data.metadata;
-// console.log(metadata)
-
-// //create variables for other demographic info
-// var id = data.metadata.id;
-// var ethnicity = data.metadata.ethnicity;
-// var gender = data.metadata.gender;
-// var age = data.metadata.age;
-// var location = data.metadata.location;
-// var bbtype = data.metadata.bbtype;
-// var wfreq = data.metadata.wfreq;
-
-// var metadata = data.metadata;
-// console.log(tableData);
-
-// Get a reference to the table body
-// var tbody = d3.select("tbody");
-
-// Step 1: Loop Through `data` and console.log each UFO report object
-// tableData.forEach(function (ufoReport) {
-//   console.log(ufoReport);
-
-//   // Step 2:  Use d3 to append one table row `tr` for each ufo report object
-//   var row = tbody.append("tr");
-
-//   // Step 3:  Use `Object.entries` to console.log each ufo report value
-//   Object.entries(ufoReport).forEach(function ([key, value]) {
-//     console.log(key, value);
-
-//     // Append a cell to the row for each value in the ufo report object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
+    Object.entries(filteredDemo).forEach((key) => {
+      panelBody.append("p").text(key[0] + ":" + key[1]);
+    });
+  });
+};
 
 dropdownmenu();
-
-
-
-
